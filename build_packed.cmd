d1432d8885d368a4e3a9da38c41c2881cb674cc4
James <james@notarealemailaddress.com>
2021-03-14 17:38:02 +0000

    .

--- build_packed.cmd
+++ build_packed.cmd
@@ -10,6 +10,8 @@ upx -9 rover-mems_%GOARCH%.exe
 REM make a zip file:
 tar.exe -a -c -f rover-mems-%GOARCH%.zip rover-mems_%GOARCH%.exe web-static
 
-REM the winrar SFX archives were getting flagged as a trojan.. I was only trying to save space
 REM make a winrar sfx:
 REM "%ProgramFiles%\WinRAR\WinRAR.exe" a -afzip -cfg- -ed -ep1 -k -m5 -r -tl "-sfx%ProgramFiles%\WinRAR\Zip.sfx" "-zsfxoptions_%GOARCH%.txt" "rover-mems-agent_%GOARCH%.exe" "web-static" "rover-mems_%GOARCH%.exe"
+
+dir rover-mems-%GOARCH%.zip
+echo "Done"
